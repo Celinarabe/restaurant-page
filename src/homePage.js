a6e648036864aff0ac7e1718c471efd6cc929f91
Celina Rabe <celinarabe@yahoo.com>
2021-02-13 12:26:18 -0600

    testing build

--- src/homePage.js
+++ src/homePage.js
@@ -9,7 +9,7 @@ let page = document.getElementById("content");
 page.innerHTML='';
 
 let restaurantTitle = document.createElement('h1');
-restaurantTitle.innerHTML = "The Arctic Cafe";
+restaurantTitle.innerHTML = "The Arctic CafeHI";
 
 let description = document.createElement('p');
 description.innerHTML = "The Arctic Cafe is an arctic-themed restaurant chain that offers sumptuous food and cool shade among the frosty icebergs of this unique eco-system. Come on down and have a try!";
